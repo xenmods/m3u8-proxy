c49e124d41bcaf177c3af90555363bb070473986
DeveloperJosh <bam0909@outlook.com>
2024-08-15 16:37:28 -0400

    refactor: Update fetch.ts to use HTTPS for baseUrl

--- src/routes/fetch.ts
+++ src/routes/fetch.ts
@@ -1,35 +1,22 @@
 import { Router, type Request, type Response } from 'express';
 import axios from 'axios';
-import { LRUCache } from 'lru-cache'
 
 const router = Router();
 
-const cache = new LRUCache<string, string | Buffer>({
-  max: 500,
-  ttl: 1000 * 60 * 5, 
-  maxSize: 5000,
-  sizeCalculation: (value, key) => {
-    return 1
-  },
-});
-
-router.get('/m3u8', async (req: Request, res: Response) => {
-  const { url } = req.query;
+router.get('/', async (req: Request, res: Response) => {
+  const { url, ref } = req.query;
 
   if (!url || typeof url !== 'string') {
     return res.status(400).json({ error: 'No URL provided' });
   }
 
-  const cachedContent = cache.get(url);
-  if (cachedContent) {
-    console.log('Returning cached m3u8 content');
-    res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
-    res.setHeader('Content-Disposition', 'inline');
-    return res.send(cachedContent);
-  }
-
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+      headers: {
+        'Referer': ref as string || '',
+      },
+    });
+
     let m3u8Content = response.data as string;
     
     const baseUrl = `https://${req.get('host')}/fetch/segment?url=`;
@@ -37,8 +24,6 @@ router.get('/m3u8', async (req: Request, res: Response) => {
     m3u8Content = m3u8Content.replace(/(.*\.ts)/g, (match) => {
       return baseUrl + encodeURIComponent(new URL(match, url).href);
     });
-
-    cache.set(url, m3u8Content);
 
     res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
     res.setHeader('Content-Disposition', 'inline');
@@ -56,22 +41,12 @@ router.get('/segment', async (req: Request, res: Response) => {
     return res.status(400).json({ error: 'No URL provided' });
   }
 
-  const cachedSegment = cache.get(url);
-  if (cachedSegment) {
-    console.log('Returning cached video segment');
-    res.setHeader('Content-Type', 'video/MP2T');
-    res.setHeader('Content-Disposition', 'inline');
-    return res.send(cachedSegment);
-  }
-
   try {
     const response = await axios({
       method: 'get',
       url,
       responseType: 'arraybuffer',
     });
-
-    cache.set(url, response.data);
 
     res.setHeader('Content-Type', 'video/MP2T');
     res.setHeader('Content-Disposition', 'inline');
