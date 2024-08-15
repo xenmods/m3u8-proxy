79b4dc08ca6decbbcf5251f6d9bbcca818f7189c
DeveloperJosh <bam0909@outlook.com>
2024-08-15 13:28:49 -0400

    old code back

--- src/routes/proxy.ts
+++ src/routes/proxy.ts
@@ -1,45 +1,7 @@
 import { Router, type Request, type Response } from 'express';
 import axios from 'axios';
-import { load } from 'cheerio';
-import path from 'path';
 
 const router = Router();
-
-router.get('/', async (req: Request, res: Response) => {
-  const { url } = req.query;
-
-  if (!url || typeof url !== 'string') {
-    return res.status(400).json({ error: 'No URL provided' });
-  }
-
-  try {
-    const response = await axios.get(url, {
-      responseType: 'text',
-    });
-
-    if (response.headers['content-type']?.includes('text/html')) {
-      const $ = load(response.data);
-
-      $('a[href], link[href], img[src], script[src]').each((_, element) => {
-        const attr = $(element).is('img') || $(element).is('script') ? 'src' : 'href';
-        const value = $(element).attr(attr);
-        if (value && value.startsWith('/')) {
-          $(element).attr(attr, new URL(value, url).toString());
-        }
-      });
-
-      res.send($.html());
-    } else {
-      // For non-HTML content, forward it as-is
-      res.set(response.headers);
-      res.send(response.data);
-    }
-  } catch (error) {
-    console.error('Error fetching the file:', (error as Error).message);
-    res.status(500).json({ error: 'Failed to proxy file' });
-  }
-});
-
 
 router.get('/m3u8', async (req: Request, res: Response) => {
   const { url } = req.query;
@@ -74,11 +36,6 @@ router.get('/segment', async (req: Request, res: Response) => {
     return res.status(400).json({ error: 'No URL provided' });
   }
 
-  // Check if the URL ends with .m3u8
-  if (!url.endsWith('.m3u8')) {
-    return res.status(400).json({ error: 'Only .m3u8 files are supported' });
-  }
-
   try {
     const response = await axios({
       method: 'get',
@@ -86,13 +43,12 @@ router.get('/segment', async (req: Request, res: Response) => {
       responseType: 'arraybuffer',
     });
 
-    // Set appropriate Content-Type for m3u8
-    res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
+    res.setHeader('Content-Type', 'video/MP2T');
     res.setHeader('Content-Disposition', 'inline');
     res.send(response.data);
   } catch (error) {
-    console.error('Error fetching the m3u8 file:', (error as Error).message);
-    res.status(500).json({ error: 'Failed to proxy m3u8 file' });
+    console.error('Error fetching the video segment:', (error as Error).message);
+    res.status(500).json({ error: 'Failed to proxy video segment' });
   }
 });
 
