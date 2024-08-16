df01682ad59104c447a6d635de86101c983a222f
DeveloperJosh <bam0909@outlook.com>
2024-08-16 15:53:39 -0400

    aded info on endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,35 @@ import { Router, type Request, type Response } from 'express';
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'NekoProxy is ready🎉' });
+  res.json(
+  { 
+    message: 'NekoProxy is ready🎉',
+    endpoints: [
+      {
+        method: 'GET',
+        usage: '/fetch',
+        description: 'Fetch a video stream from a URL',
+        query: {
+          url: 'The URL of the video or image.',
+          ref: 'The referrer URL'
+        }
+      },
+      {
+        method: 'GET',
+        usage: '/fetch/segment',
+        description: 'Fetch a video segment from a URL',
+        query: {
+          url: 'The URL of the video segment.'
+        }
+      },
+      {
+        method: 'GET',
+        usage: '/health',
+        description: 'Check the health status of the server'
+      }
+    ]
+   }
+  );
 });
 
 router.get('/health', (req: Request, res: Response) => {
