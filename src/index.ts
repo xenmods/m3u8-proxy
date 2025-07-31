28310e366ce9fb487f8f886f46a5cd74de70bc39
Ilf Momin <70523287+xenmods@users.noreply.github.com>
2025-07-31 10:35:00 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,36 @@ import proxyRouter from './routes/fetch.ts';
 
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use(cors({
-  origin: '*',
+// app.use(cors({
+//   origin: [],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   maxAge: 3600,
+// }));
+// List of domains you want to allow
+const allowedOrigins = [
+  'https://animerealms.org',
+  'https://www.animerealms.org'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   maxAge: 3600,
-}));
+};
+
+// Use the new options in your app
+app.use(cors(corsOptions));
 
 app.use(morgan('combined'));
 
