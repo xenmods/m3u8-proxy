15c796ad54a8bb06d0b072cab39ec43bba6318f3
Ilf Momin <70523287+xenmods@users.noreply.github.com>
2025-07-31 11:03:59 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,37 +7,37 @@ import proxyRouter from './routes/fetch.ts';
 
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.use(cors({
-//   origin: [],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   maxAge: 3600,
-// }));
-// List of domains you want to allow
-const allowedOrigins = [
-  'https://animerealms.org',
-  'https://www.animerealms.org',
-  'https://literate-parakeet-xgq4www6q6439xgq-3000.app.github.dev'
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
+app.use(cors({
+  origin: [],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   maxAge: 3600,
-};
+}));
+// List of domains you want to allow
+// const allowedOrigins = [
+//   'https://animerealms.org',
+//   'https://www.animerealms.org',
+//   'https://literate-parakeet-xgq4www6q6439xgq-3000.app.github.dev'
+// ];
 
-// Use the new options in your app
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   maxAge: 3600,
+// };
+
+// // Use the new options in your app
+// app.use(cors(corsOptions));
 
 app.use(morgan('combined'));
 
