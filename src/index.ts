f16b0af9bbbfa251e1c34e2dad4bbf310f53d829
Ilf Momin <70523287+xenmods@users.noreply.github.com>
2025-07-31 11:05:43 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: [],
+  origin: "*",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   maxAge: 3600,
